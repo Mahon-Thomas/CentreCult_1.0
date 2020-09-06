3089d693333b8f07fcae26d16bae951a63ae979e
MAHON Thomas <54801141+Mahon-Thomas@users.noreply.github.com>
2020-09-06 14:54:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -41,148 +41,155 @@
 
 
             }
+
+
             
-            if ($_REQUEST['action'] == "Interface"){
+            if(isset($_SESSION['userId'])){
+                if ($_REQUEST['action'] == "Interface"){
 
-                include "vue/Interface.php";
+                    include "vue/Interface.php";
+        
+                }
 
-            }
+                if($_REQUEST['action'] == "Deconnexion") {
 
-            if($_REQUEST['action'] == "Deconnexion") {
+                    session_destroy();
 
-                session_destroy();
+                header("Location: ./");
+                }
 
-            header("Location: index.php?action=Accueil");
-            }
-
-            if($_REQUEST['action'] == "Accueil"){
-                
-                include "./vue/Accueil.php";
-            }
-            
-
-            // PARTIE AJOUT UTILISATEUR
-            if($_REQUEST['action'] == "formAdd"){
-                include "./vue/user/form-user.php";
-            }
-
-            if($_REQUEST['action'] == "addUser"){
-
-                $uti->adduser($_POST);
-
-                header ("Location: index.php?action=Interface");
-                
-            }
-            // FIN AJOUT UTILISATEUR
-
-            // PARTIE EDIT UTILISATEUR
-            if($_REQUEST['action'] == 'tbl-edit') {
+                if($_REQUEST['action'] == "Accueil"){
+                    
+                    include "./vue/Accueil.php";
+                }
                 
 
-                $tbluser = $uti->getUsers();
-                include "./vue/user/edit-user.php";
-            }
+                // PARTIE AJOUT UTILISATEUR
+                if($_REQUEST['action'] == "formAdd"){
+                    include "./vue/user/form-user.php";
+                }
 
-            if($_REQUEST['action'] == 'Modifier'){
-                $resUser = $uti->getUser($_POST);
-                include "./vue/user/editform-user.php";
+                if($_REQUEST['action'] == "addUser"){
 
-            }
+                    $uti->adduser($_POST);
 
-            if($_REQUEST['action'] == 'edituser'){
-                $uti->edituser($_POST);
-                $tbluser = $uti->getUsers();
-                include "./vue/user/edit-user.php";
-            }
-            // FIN EDIT UN UTILISATEUR
+                    header ("Location: index.php?action=Interface");
+                    
+                }
+                // FIN AJOUT UTILISATEUR
 
-            // PARTIE SUPPRIMER UN UTILISATEUR
-            if($_REQUEST['action'] == 'tbl-delete-user') {
-                $tbluser = $uti->getUsers();
-                include "./vue/user/delete-user.php";
-            }
+                // PARTIE EDIT UTILISATEUR
+                if($_REQUEST['action'] == 'tbl-edit') {
+                    
 
-            if($_REQUEST['action'] == 'Supprimer'){
-                $_SESSION['supUser'] = $_POST['ide'];
-                $resUser = $uti->getUser($_POST);
-                
-                include './vue/user/verif-delete-user.php';
-            }
+                    $tbluser = $uti->getUsers();
+                    include "./vue/user/edit-user.php";
+                }
 
-            if($_REQUEST['action'] == 'confirm-delete'){
+                if($_REQUEST['action'] == 'Modifier'){
+                    $resUser = $uti->getUser($_POST);
+                    include "./vue/user/editform-user.php";
 
-                $id = $_SESSION['supUser'];
-                unset($_SESSION['supUser']);
+                }
 
-                $uti->deleteUser($id);
-                header ('Location: index.php?action=tbl-delete-user');
-            }
-            // FIN SUPPRIMER UN UTILISATEUR
+                if($_REQUEST['action'] == 'edituser'){
+                    $uti->edituser($_POST);
+                    $tbluser = $uti->getUsers();
+                    include "./vue/user/edit-user.php";
+                }
+                // FIN EDIT UN UTILISATEUR
 
-            // PARTIE AJOUT UN POST
-            if($_REQUEST['action'] == 'form-add-post'){
-                include "./vue/poste/form-add-poste.php";
-            }
+                // PARTIE SUPPRIMER UN UTILISATEUR
+                if($_REQUEST['action'] == 'tbl-delete-user') {
+                    $tbluser = $uti->getUsers();
+                    include "./vue/user/delete-user.php";
+                }
 
-            if($_REQUEST['action'] == "addPoste"){
+                if($_REQUEST['action'] == 'Supprimer'){
+                    $_SESSION['supUser'] = $_POST['ide'];
+                    $resUser = $uti->getUser($_POST);
+                    
+                    include './vue/user/verif-delete-user.php';
+                }
 
-                $respost = $post->getRefPoste($_POST);
+                if($_REQUEST['action'] == 'confirm-delete'){
 
-                if(empty($respost)){
-                $post->addpost($_POST);
+                    $id = $_SESSION['supUser'];
+                    unset($_SESSION['supUser']);
 
-                header ("Location: index.php?action=Interface");
-                }else{
-                    $unique = "La référence du poste existe déjà ! ";
+                    $uti->deleteUser($id);
+                    header ('Location: index.php?action=tbl-delete-user');
+                }
+                // FIN SUPPRIMER UN UTILISATEUR
+
+                // PARTIE AJOUT UN POST
+                if($_REQUEST['action'] == 'form-add-post'){
                     include "./vue/poste/form-add-poste.php";
                 }
+
+                if($_REQUEST['action'] == "addPoste"){
+
+                    $respost = $post->getRefPoste($_POST);
+
+                    if(empty($respost)){
+                    $post->addpost($_POST);
+
+                    header ("Location: index.php?action=Interface");
+                    }else{
+                        $unique = "La référence du poste existe déjà ! ";
+                        include "./vue/poste/form-add-poste.php";
+                    }
+                }
+                // FIN AJOUT UN POSTE
+
+                // PARTIE MODIFIER UN POSTE
+                if ($_REQUEST['action'] == "tbl-edit-post"){
+                    $tblpost = $post->getPostes();
+                    include "./vue/poste/edit-poste.php";
+                }
+
+                if($_REQUEST['action'] == 'Modifier le poste'){
+                    $resPost = $post->getPoste($_POST);
+                    include "./vue/poste/editform-poste.php";
+
+                }
+
+                if($_REQUEST['action'] == 'editposte'){
+
+                    $post->editpost($_POST);
+                    $tblpost = $post->getPostes();
+                    include "./vue/poste/edit-poste.php";
+
+                }
+                // FIN MODIFIER UN POSTE
+
+                // PARTIE SUPPRIMER UN POSTE
+                if($_REQUEST['action'] == 'tbl-delete-poste') {
+                    $tblpost = $post->getPostes();
+                    include "./vue/poste/delete-poste.php";
+                }
+
+                if($_REQUEST['action'] == 'Supprimer le poste'){
+                    $_SESSION['supPost'] = $_POST['ide'];
+                    $resPost = $post->getPoste($_POST);
+                    
+                    include './vue/poste/verif-delete-poste.php';
+                }
+
+                if($_REQUEST['action'] == 'confirm-delete-poste'){
+
+                    $id = $_SESSION['supPost'];
+                    unset($_SESSION['supPost']);
+
+                    $post->deletePost($id);
+                    header ('Location: index.php?action=tbl-delete-poste');
+                }
+                // FIN SUPPRIMER UN POSTE
+            }else{
+
+                header("Location: ./");
+            
             }
-            // FIN AJOUT UN POSTE
-
-            // PARTIE MODIFIER UN POSTE
-            if ($_REQUEST['action'] == "tbl-edit-post"){
-                $tblpost = $post->getPostes();
-                include "./vue/poste/edit-poste.php";
-            }
-
-            if($_REQUEST['action'] == 'Modifier le poste'){
-                $resPost = $post->getPoste($_POST);
-                include "./vue/poste/editform-poste.php";
-
-            }
-
-            if($_REQUEST['action'] == 'editposte'){
-
-                 $post->editpost($_POST);
-                 $tblpost = $post->getPostes();
-                include "./vue/poste/edit-poste.php";
-
-            }
-            // FIN MODIFIER UN POSTE
-
-            // PARTIE SUPPRIMER UN POSTE
-            if($_REQUEST['action'] == 'tbl-delete-poste') {
-                $tblpost = $post->getPostes();
-                include "./vue/poste/delete-poste.php";
-            }
-
-            if($_REQUEST['action'] == 'Supprimer le poste'){
-                $_SESSION['supPost'] = $_POST['ide'];
-                $resPost = $post->getPoste($_POST);
-                
-                include './vue/poste/verif-delete-poste.php';
-            }
-
-            if($_REQUEST['action'] == 'confirm-delete-poste'){
-
-                $id = $_SESSION['supPost'];
-                unset($_SESSION['supPost']);
-
-                $post->deletePost($id);
-                header ('Location: index.php?action=tbl-delete-poste');
-            }
-            // FIN SUPPRIMER UN POSTE
-
         include "vue/Footer.php";
 
         }else{
